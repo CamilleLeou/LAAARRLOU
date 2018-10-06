36849d36321d96be5f6a8d931613b17adfe5e376
Cutajife <32271952+Cutajife@users.noreply.github.com>
2018-10-06 17:06:49 +0200

    mise en place de mysql
    Ne fonctionne pas tant que la bibliothèque mysql n'est pas installée et que le serveur n'est pas connecté

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <winsock.h>
+#include <MYSQL/mysql.h>
 
 typedef struct Film Film;
 typedef struct People People;
@@ -169,27 +171,66 @@ void afficher(Film Film)
 
 int main()
 {
-    /*FILE* fichier = NULL;
-    fichier = fopen(const char* liste, const char* r+);
 
-    if(fichier == NULL)
-    {
-        printf("Error \n");
+
+    MYSQL mysql;
+
+    mysql_init(&mysql);
+    mysql_options(&mysql, MYSQL_READ_DEFAULT_GROUP, "option");
+    if(mysql_real_connect(&mysql, "127.0.0.1", "root", "", "film_tp5", 0, NULL, 0)){
+
+        //Requête qui sélectionne tout dans ma table scores
+        mysql_query(&mysql, "SELECT * FROM acteurs_realisateurs");
+
+        //Déclaration des objets
+        MYSQL_RES *result = NULL;
+        MYSQL_ROW row;
+
+        unsigned int i = 0;
+        unsigned int num_champs = 0;
+
+        //On met le jeu de résultat dans le pointeur result
+        result = mysql_use_result(&mysql);
+
+        //On récupère le nombre de champs
+        num_champs = mysql_num_fields(result);
+
+        //Tant qu'il y a encore un résultat ...
+        while ((row = mysql_fetch_row(result)))
+        {
+            //On déclare un pointeur long non signé pour y stocker la taille des valeurs
+            unsigned long *lengths;
+
+            //On stocke ces tailles dans le pointeur
+            lengths = mysql_fetch_lengths(result);
+
+            //On fait une boucle pour avoir la valeur de chaque champs
+            for(i = 0; i < num_champs; i++)
+            {
+               //On ecrit toutes les valeurs
+               printf("[%.*s] ", (int) lengths[i], row[i] ? row[i] : "NULL");
+            }
+            printf("\n");
+        }
+
+        //Libération du jeu de résultat
+        mysql_free_result(result);
+
+        //Fermeture de MySQL
+        mysql_close(&mysql);
+
     }
-    else
-    {
+    else {
 
+        printf("Echec de connexion au server");
     }
-
-    fclose(fichier); */
-
-    Film Titanic;
+    /*Film Titanic;
 
     Titanic = addFilm(Titanic);
 
     system("cls");
 
-    afficher(Titanic);
+    afficher(Titanic);*/
 
     return 0;
 }
